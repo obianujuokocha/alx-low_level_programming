91ed636f3bc5b6b5ec4ffbcb564db6abff95a566
obianujuokocha <obianujuokocha@yahoo.com>
2022-07-06 08:34:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+* printf("with proper grammar, but the outcome is a place of art,\n");
+* return (0);
+}
