85ab07b9af578376d209e45ffc4fc28a5e843de1
obianujuokocha <obianujuokocha@yahoo.com>
2022-07-06 08:12:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 /**
  * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
- * */
-int main (void)
+ */
+int main(void)
 {
-	puts("/"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return(0);
 }
