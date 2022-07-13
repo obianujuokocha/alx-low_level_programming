ba34feebfa8bc14ac58e67f3184514568a5775b3
obianujuokocha <obianujuokocha@yahoo.com>
2022-07-12 20:50:31 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - A function that prints a string starting from the first character
+ *
+ * @str: string to be checked
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	i = strlen(str);
+	for (j = 0; j < 1; j +=2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
