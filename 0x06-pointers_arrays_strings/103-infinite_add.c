bba2c2f50a59ab32168185711efd10538ade8c1c
obianujuokocha <obianujuokocha@yahoo.com>
2022-07-14 22:34:04 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,7 +22,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		num = (*n1 - '0') + (*n2 - '0');
 		num += tens;
 		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
+		tens = num  10;
 	}
 
 	for (; *n1; n1--, r_index--)
