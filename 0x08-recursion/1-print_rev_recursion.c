77dd85628f65e6fc224165c63fc9dbcd20145004
obianujuokocha <obianujuokocha@yahoo.com>
2022-07-20 19:44:05 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recurson - print a string in reverse
+ * _print_rev_recursion - print a string in reverse
  * @s:a pointer to the string to print in reverse
  *
  * Return: void
