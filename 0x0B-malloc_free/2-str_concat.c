c820bf6312ef8a7cba5ac6b3ec87f0611069d4d2
obianujuokocha <obianujuokocha@yahoo.com>
2022-07-27 01:42:08 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,29 +1,87 @@
 #include "main.h"
+
 #include <stdlib.h>
+
+
+
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * @str: string
- *
- * Return: pointer of an array of chars
+
+ * str_concat - Write a function that concatenates two strings
+
+ * @s1: first strign
+
+ * @s2: second string
+
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+
+ * Otherwise - a pointer to the 2-dimensional array of integers.
+
  */
-char *_strdup(char *str)
+
+
+
+char *str_concat(char *s1, char *s2)
+
 {
+
 	char *strout;
-	unsigned int i, j;
 
-	if (str == NULL)
-		return (NULL);
+	unsigned int i, j, k, limit;
 
-	for (i = 0; str[i] != '\0'; i++)
+
+
+	if (s1 == NULL)
+
+		s1 = "";
+
+	if (s2 == NULL)
+
+		s2 = "";
+
+
+
+	for (i = 0; s1[i] != '\0'; i++)
+
 		;
 
-	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+
+	for (j = 0; s2[j] != '\0'; j++)
+
+		;
+
+
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+
 
 	if (strout == NULL)
+
+	{
+
+		free(strout);
+
 		return (NULL);
 
-	for (j = 0; j <= i; j++)
-		strout[j] = str[j];
+	}
 
-			return (strout);
+
+
+	for (k = 0; k < i; k++)
+
+		strout[k] = s1[k];
+
+
+
+	limit = j;
+
+	for (j = 0; j <= limit; k++, j++)
+
+		strout[k] = s2[j];
+
+
+
+	return (strout);
+
 }
