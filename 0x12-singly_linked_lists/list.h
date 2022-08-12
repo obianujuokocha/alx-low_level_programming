23b5589cc89f51e5db312d752b22665aa0fffb32
obianujuokocha <obianujuokocha@yahoo.com>
2022-08-11 20:33:36 -0700

    task 0

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,13 +1,6 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-/*
-* Desc: header file containinf prototypes and definitions
-*	for all functions and types written in this proj
-*/
-
-#include <stdlib.h>
-
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,8 +8,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for Holberton project
- */
+*/
 typedef struct list_s
 {
 	char *str;
@@ -30,4 +22,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
